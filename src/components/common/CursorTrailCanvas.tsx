536456d70d4a0f1210a4caebc9b43aa6b69fde3c
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-07-20 13:26:49 +0500

    Custom Cursor

---
+++ src/components/common/CursorTrailCanvas.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { CSSProperties, useEffect, useRef } from "react";
+import { cursorTrail } from "@/utility/cursorTrail";
+
+export type CursorTrailCanvasProps = {
+  color: string;
+  className?: string;
+  style?: CSSProperties;
+};
+
+export default function CursorTrailCanvas(props: CursorTrailCanvasProps) {
+  const refCanvas = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const { cleanUp, renderTrailCursor } = cursorTrail({
+      ref: refCanvas,
+      color: props.color,
+    });
+    renderTrailCursor();
+
+    return () => {
+      cleanUp();
+    };
+  }, [props.color]);
+
+  return (
+    <canvas
+      ref={refCanvas}
+      className={props.className}
+      style={{ ...props.style, zIndex: 1000 }}
+    ></canvas>
+  );
+}
