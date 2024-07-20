1b238fc7d11beb126043e788e827786ccd9849a8
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-07-20 16:26:10 +0500

    Expertise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/home/Banner";
 import { Expertise } from "@/components/home/Expertise";
+// import { ExpertiseCloudIcon } from "@/components/home/ExpertiseCloudIcons";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <Expertise />
+      {/* <ExpertiseCloudIcon /> */}
     </>
   );
 }
